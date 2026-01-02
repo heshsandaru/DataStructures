bc91ef2d0d238fa7cc7306626f96366da8c7c660
Heshan <heshansandaruwan999@gmail.com>
2026-01-02 13:07:12 +0530

    Add array declaration, initialization, and multiple XML configuration files

--- Arrays_01.java
+++ Arrays_01.java
@@ -17,5 +17,23 @@ public class Arrays_01 {
         for (int number : numbers) {
             System.out.print(number + " ");
         }
+
+
+        /*////////////////////*/
+
+        // Declaration and Initialization
+        int[] arr = new int[5]; // Creating an integer array of size 5
+
+        // Accessing elements
+        arr[0] = 10; // Assigning value 10 to the first element
+        int value = arr[2]; // Accessing the third element
+        System.out.println("value: " + value);
+
+        // Iterating through the array
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
+
     }
 }
