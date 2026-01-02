50bd77c8207dc233f70505ae48376374be091c0b
Heshan <heshansandaruwan999@gmail.com>
2026-01-02 13:15:31 +0530

    Add examples for HashMap usage and new XML configuration files

--- HashMap_03.java
+++ HashMap_03.java
@@ -4,5 +4,10 @@ public class HashMap_03 {
     public static void main(String[] args) {
         // Declaration and Initialization
         HashMap<String, Integer> map = new HashMap<>();
+
+        // Adding key-value pairs
+        map.put("Apple", 1);
+        map.put("Banana", 2);
+        map.put("Orange", 3);
     }
 }
