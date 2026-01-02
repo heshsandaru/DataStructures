8f040f84da5a249ace5f806eb97e685cd699e0a4
Heshan <heshansandaruwan999@gmail.com>
2026-01-02 13:15:03 +0530

    Add HashMap declaration and initialization example

---
+++ HashMap_03.java
@@ -0,0 +1,8 @@
+import java.util.HashMap;
+
+public class HashMap_03 {
+    public static void main(String[] args) {
+        // Declaration and Initialization
+        HashMap<String, Integer> map = new HashMap<>();
+    }
+}
