a3f3615b15dc1c0cb3d871c15787134775df575d
Heshan <heshansandaruwan999@gmail.com>
2026-01-02 13:04:29 +0530

    array Declaration and Initialization print Array

---
+++ Arrays_01.java
@@ -0,0 +1,12 @@
+public class Arrays_01 {
+    public static void main(String[] args) {
+        int[] numbers = {5, 3, 8, 1, 2};
+
+        // Print original array
+        System.out.println("Original array:");
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+
+    }
+}
