7094e3e48383041201f05073e4368bba3f4628f9
Heshan <heshansandaruwan999@gmail.com>
2026-01-02 13:11:43 +0530

    Add ArrayList example with declaration, initialization, and element access

---
+++ ArrayList_02.java
@@ -0,0 +1,19 @@
+// Importing ArrayList
+import java.util.ArrayList;
+public class ArrayList_02 {
+
+    public static void main(String[] args) {
+        // Declaration and Initialization
+        ArrayList<Integer> list = new ArrayList<>(); // Creating an ArrayList of Integers
+
+        // Adding elements
+        list.add(10); // Adding value 10 to the list
+        list.add(20);
+        list.add(30);
+
+        // Accessing elements
+        int value = list.get(1); // Accessing the second element
+        System.out.println("value: " + value);
+
+    }
+}
